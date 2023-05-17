3c9e40d8094c4b6e5d5da0eab4f92b33897491be
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-05-17 15:29:48 -0500

    Fix: progress width

--- src/components/layout/ProgressBar.tsx
+++ src/components/layout/ProgressBar.tsx
@@ -6,7 +6,8 @@ function ProgressBar({ current }: ProgressBarProps) {
   return (
     <div className='relative h-6 w-2/4 overflow-hidden rounded-full border border-white text-black transition-all duration-300'>
       <span
-        className={`absolute h-full w-[${current}px] bg-white text-center text-sm`}
+        className={`absolute flex h-full items-center justify-center bg-white text-center text-sm`}
+        style={{ width: `${current}%` }}
       >
         {current}%
       </span>
