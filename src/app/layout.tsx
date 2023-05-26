8f14e18c75f0da40e9870244a950c1fdb3ad4833
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-26 15:19:30 -0500

    Add: React query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import Navigation from '@/components/navigation';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './globals.css';
 
 export const metadata = {
   title: 'Alejandro LR',
   description: 'Alejandro LR is a software engineer based on Lima, Peru.',
 };
+
+const queryClient = new QueryClient()
 
 export default function RootLayout({
   children,
@@ -14,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='flex min-h-screen flex-col gap-2 bg-slate-900 p-4 lg:mx-auto lg:max-w-screen-md xl:mx-auto xl:max-w-screen-md'>
-        <Navigation />
-        <main className='flex-1'>{children}</main>
+        <QueryClientProvider client={queryClient}>
+          <Navigation />
+          <main className='flex-1'>{children}</main>
+        </QueryClientProvider>
       </body>
     </html>
   );
