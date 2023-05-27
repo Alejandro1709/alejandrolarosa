b1c7f5345ee57c7968ab056c34b9ae0bb8ee75fd
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-27 12:44:37 -0500

    Add: Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Navigation from '@/components/navigation';
 import QueryProvider from '@/components/QueryProvider';
+import Footer from '@/components/navigation/Footer';
 import './globals.css';
 
 export const metadata = {
@@ -14,10 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className='flex min-h-screen flex-col gap-2 bg-slate-900 p-4 lg:mx-auto lg:max-w-screen-md xl:mx-auto xl:max-w-screen-md'>
+      <body className='relative flex min-h-screen flex-col gap-2 bg-slate-900 p-4 lg:mx-auto lg:max-w-screen-md xl:mx-auto xl:max-w-screen-md'>
         <QueryProvider>
           <Navigation />
           <main className='flex-1'>{children}</main>
+          <Footer />
         </QueryProvider>
       </body>
     </html>
