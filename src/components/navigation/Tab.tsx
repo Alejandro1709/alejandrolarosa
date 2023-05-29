10bfb53d14a927aff0e96e9efd67be69c107b12f
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-29 12:58:13 -0500

    Add: Next routing

--- src/components/navigation/Tab.tsx
+++ src/components/navigation/Tab.tsx
@@ -1,14 +1,16 @@
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import type ITab from '@/types/tab';
 
 type NavLinkProps = {
   tab: ITab;
-  active: ITab;
 };
 
-function Tab({ tab, active }: NavLinkProps) {
+function Tab({ tab }: NavLinkProps) {
+  const pathName = usePathname()
+
   return (
-    <Link href={tab.href} className={`text-md flex-1 cursor-pointer border ${active.active && active.name === tab.name
+    <Link href={tab.href} className={`text-md flex-1 cursor-pointer border ${pathName === tab.href
       ? 'hover:border-white'
       : 'border-slate-700'
       } bg-slate-800 p-2.5 text-center hover:border-white`}>
--- src/components/navigation/Tabs.tsx
+++ src/components/navigation/Tabs.tsx
@@ -1,23 +1,15 @@
 'use client';
 
-import { useState } from 'react';
 import tabs from '@/data/tabs';
-import { useTabStore } from '@/stores/tabStore';
 import NavLink from './Tab';
-import type ITab from '@/types/tab';
 
 function Tabs() {
-  const currentTab = useTabStore((state) => state.currentTab);
-
-  const [activeTab, setActiveTab] = useState<ITab>(tabs[currentTab - 1]);
-
   return (
     <ul className='flex w-full flex-row justify-center items-start gap-2 transition-all duration-200'>
       {tabs.map((tab) => (
         <NavLink
           key={tab.name}
           tab={tab}
-          active={activeTab}
         />
       ))}
     </ul>
