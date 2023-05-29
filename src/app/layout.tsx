f0c9e0bfd6a09789746e0a1ff02eae009567ac80
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-29 12:14:16 -0500

    Add: more metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,14 @@ import Footer from '@/components/navigation/Footer';
 import './globals.css';
 
 export const metadata = {
+  generator: 'Next.js',
   title: 'Alejandro LR',
   description: 'My name is Alejandro LR, a talented software engineer based in Lima, Peru. With expertise in various programming languages and a passion for creating efficient and scalable solutions, I strive to deliver high-quality software that exceeds expectations. Explore my portfolio and discover how I can help you transform your ideas into reality.',
+  keywords: ['Portfolio', 'Enigineer', 'Full Stack', 'Web Dev'],
+  authors: [{ name: 'Alejandro LR' }],
+  colorScheme: 'dark',
+  creator: 'Alejandro LR',
+  publisher: 'Alejandro LR'
 };
 
 export default function RootLayout({
