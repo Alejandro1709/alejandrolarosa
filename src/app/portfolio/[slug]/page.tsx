7b7bb85acacd042a59b9220aeb656e4e4a44d6ee
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-06-21 15:35:05 -0500

    Add: grid

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,23 +1,45 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Alejandro LR | Get That Home',
-  description: ''
+  description: '',
 };
 
 function ProjectPage() {
   return (
-    <article className="flex flex-col gap-2">
-      <header className="flex flex-col gap-2">
-        <div className="h-[135px] bg-slate-800 border border-slate-700">IMAGE</div>
-        <div className="flex flex-row gap-2">
-          <Link href='/portfolio' className="text-md flex-1 cursor-pointer border border-slate-700 bg-slate-800 p-2.5 text-center hover:border-white">Go Back</Link>
-          <button className="text-md flex-1 cursor-pointer border border-slate-700 bg-slate-800 p-2.5 text-center hover:border-white">Open Project</button>
-          <button className="text-md flex-1 cursor-pointer border border-slate-700 bg-slate-800 p-2.5 text-center hover:border-white">Github Repo</button>
+    <article className='flex flex-col gap-2'>
+      <header className='flex flex-col gap-2'>
+        <div className='h-[135px] border border-slate-700 bg-slate-800'>
+          IMAGE
+        </div>
+        <div className='flex flex-row gap-2'>
+          <Link
+            href='/portfolio'
+            className='text-md flex-1 cursor-pointer border border-slate-700 bg-slate-800 p-2.5 text-center hover:border-white'
+          >
+            Go Back
+          </Link>
+          <button className='text-md flex-1 cursor-pointer border border-slate-700 bg-slate-800 p-2.5 text-center hover:border-white'>
+            Open Project
+          </button>
+          <button className='text-md flex-1 cursor-pointer border border-slate-700 bg-slate-800 p-2.5 text-center hover:border-white'>
+            Github Repo
+          </button>
         </div>
       </header>
+      <section className=''>
+        <div className='flex flex-col gap-1.5'>
+          <h1 className='text-3xl font-medium'>
+            Get That Home - Rent a house near you.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente,
+            ducimus?
+          </p>
+        </div>
+      </section>
     </article>
-  )
+  );
 }
 
-export default ProjectPage
+export default ProjectPage;
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,16 +1,20 @@
+import Link from 'next/link';
+
 function Grid() {
   return (
     <ul className='grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3'>
-      <li className='h-[133px] rounded-md border border-slate-700 bg-slate-800 hover:scale-95 transition-all duration-200 cursor-pointer'>
+      <Link href='/portfolio/fjfjd'>
+        <li className='h-[133px] cursor-pointer rounded-md border border-slate-700 bg-slate-800 transition-all duration-200 hover:scale-95'>
+          Hello
+        </li>
+      </Link>
+      <li className='h-[133px] cursor-pointer rounded-md border border-slate-700 bg-slate-800 transition-all duration-200 hover:scale-95'>
         Hello
       </li>
-      <li className='h-[133px] rounded-md border border-slate-700 bg-slate-800 hover:scale-95 transition-all duration-200 cursor-pointer'>
+      <li className='h-[133px] cursor-pointer rounded-md border border-slate-700 bg-slate-800 transition-all duration-200 hover:scale-95'>
         Hello
       </li>
-      <li className='h-[133px] rounded-md border border-slate-700 bg-slate-800 hover:scale-95 transition-all duration-200 cursor-pointer'>
-        Hello
-      </li>
-      <li className='h-[133px] rounded-md border border-slate-700 bg-slate-800 hover:scale-95 transition-all duration-200 cursor-pointer'>
+      <li className='h-[133px] cursor-pointer rounded-md border border-slate-700 bg-slate-800 transition-all duration-200 hover:scale-95'>
         Hello
       </li>
     </ul>
