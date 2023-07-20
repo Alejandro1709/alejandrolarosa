4ae428ef768c590cec58ccf3a3f6bc9489171566
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-07-20 14:44:43 -0500

    Add: Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 export default function Home() {
   return (
-    <main className='mx-8'>
-      <section id='hero' className='my-8 py-2'>
+    <div className='mx-6'>
+      <section id='hero' className='my-8 flex flex-col items-center'>
         <header className='flex flex-col items-center gap-6 font-bold uppercase text-white'>
-          <h1 className='text-center text-4xl leading-snug tracking-wider'>
-            <span className='mr-1'>👋</span> Hello, my name is Alejandro.
-          </h1>
+          <div className='flex flex-col items-center gap-6 text-4xl'>
+            <span className='animate-wiggle animate-infinite'>👋</span>
+            <h1 className='text-center leading-snug tracking-wider'>
+              Hello, my name is Alejandro.
+            </h1>
+          </div>
           <h3 className='bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-2xl text-transparent'>
             I&apos;m a software engineer.
           </h3>
         </header>
       </section>
-    </main>
+    </div>
   );
 }
