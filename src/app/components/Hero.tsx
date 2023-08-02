23e9404654bdce68dff4a5cbff3f2022ca48e987
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-08-02 12:49:05 -0500

    Upgrade: npm version and Change: some ui stuff

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -63,7 +63,7 @@ function Hero() {
   return (
     <section
       id='hero'
-      className='mt-8 flex flex-col items-center gap-8 lg:mt-28 lg:flex-row lg:gap-20'
+      className='mt-16 flex flex-col items-center gap-8 lg:mt-28 lg:flex-row lg:gap-20'
     >
       <header className='flex flex-col items-center gap-6 text-center font-bold uppercase text-white lg:w-[65%] lg:gap-8 lg:text-left'>
         <h1 className='text-4xl leading-snug tracking-wider sm:text-5xl md:text-6xl'>
@@ -72,7 +72,7 @@ function Hero() {
         <h3 className='animate-pulse bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-2xl text-transparent sm:text-3xl md:text-4xl lg:self-start lg:text-5xl'>
           I&apos;m a software engineer.
         </h3>
-        <div className='flex flex-shrink-0 flex-row gap-8 lg:mt-4 lg:self-start'>
+        <div className='mt-4 flex flex-shrink-0 flex-row gap-8 lg:self-start'>
           <button className='rounded-md bg-purple-400 px-4 py-2 transition-all hover:bg-indigo-300 active:scale-95'>
             Hire Me!
           </button>
