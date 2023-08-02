0c87a30f6aac49a04fd3e44814b3762a72e6d7e3
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-08-02 12:30:34 -0500

    Add: wip notice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,25 @@ import Hero from './components/Hero';
 
 export default function Home() {
   return (
-    <div className='mx-6 md:mx-auto md:max-w-screen-md lg:mx-auto lg:max-w-screen-lg'>
+    <div className='mx-6 space-y-14 md:mx-auto md:max-w-screen-md lg:mx-auto lg:max-w-screen-lg'>
       <Hero />
-      {/* <section>
-        <h2 className='text-center text-3xl font-medium leading-snug tracking-wider'>
-          Who am i?
-        </h2>
-      </section> */}
+      <section>
+        <header className='flex flex-col items-center justify-center text-3xl leading-snug tracking-wider lg:text-left'>
+          <div className='flex justify-center gap-2 font-medium'>
+            <span className='bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-transparent'>
+              **
+            </span>
+            <h3>Work in progress</h3>
+            <span className='bg-gradient-to-r from-indigo-300 to-purple-400 bg-clip-text text-transparent'>
+              **
+            </span>
+          </div>
+          <p className='mt-4 max-w-sm text-center text-sm'>
+            This site is still under construction. Please check back later for
+            more content.
+          </p>
+        </header>
+      </section>
     </div>
   );
 }
