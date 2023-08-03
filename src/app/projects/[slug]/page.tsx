96f9f25cc584247cc556430c5940371b886e10a5
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-08-03 15:17:10 -0500

    Add: title to project page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,13 +1,25 @@
 import Button from '@/app/components/ui/Button';
-import Link from 'next/link';
+import NavLink from '@/app/components/ui/NavLink';
 
 function ProjectPage() {
   return (
-    <div className='mt-10 flex flex-col items-center justify-center gap-8 lg:mt-16 lg:flex-row lg:gap-20'>
-      <header className='flex w-full flex-row items-center justify-between px-10 lg:w-[65%]'>
-        <Link href='/'>Back</Link>
-        <h2 className='text-2xl'>Project Title</h2>
-        <Button className='bg-[#181818]'>Share</Button>
+    <div className='mx-auto mt-10 flex flex-col items-center justify-center gap-4 lg:mt-16 lg:max-w-screen-lg lg:flex-row lg:gap-20'>
+      <header className='flex w-full flex-col gap-8 px-10'>
+        <div className='flex flex-row items-center justify-between'>
+          <NavLink href='/' className='bg-[#181818] hover:bg-[#252525]'>
+            Back
+          </NavLink>
+          <h2 className='text-2xl font-medium'>Project Details</h2>
+          <Button className='bg-[#181818] hover:bg-[#252525]'>Share</Button>
+        </div>
+
+        <div className='h-48 rounded-md bg-[#181818]'></div>
+
+        <div className='ml-1 flex flex-col gap-2'>
+          <h1 className='text-3xl font-bold leading-snug tracking-wider'>
+            Get That Home
+          </h1>
+        </div>
       </header>
     </div>
   );
