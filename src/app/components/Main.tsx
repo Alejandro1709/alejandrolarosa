d2bdcc30bf239696fb623a12612eec37cbcd23b7
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-08-07 16:34:43 -0500

    Add: Experience section and card component

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useInView } from 'react-intersection-observer';
+import Experience from './Experience';
 import Footer from './Footer';
 import Hero from './Hero';
 import Projects from './Projects';
@@ -19,6 +20,7 @@ function Main() {
       <div className='mx-6 space-y-14 md:mx-auto md:max-w-screen-md lg:mx-auto lg:max-w-screen-lg'>
         <Hero id='hero' ref={heroRef} />
         <Projects id='projects' />
+        <Experience id='experience' />
         <Footer />
       </div>
     </>
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -7,7 +7,7 @@ type ProjectsProps = {
 function Projects({ id }: ProjectsProps) {
   return (
     <section id={id} className='flex flex-col items-center space-y-10'>
-      <h2 className='text-3xl font-medium uppercase leading-snug tracking-wider text-purple-400/60 lg:self-start'>
+      <h2 className='text-3xl font-medium uppercase leading-snug tracking-wider text-white lg:self-start'>
         projects
       </h2>
       <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3'>
