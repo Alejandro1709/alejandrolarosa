22e689aec636a3e769e8f32f80da6fba5f1c6c97
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-08-15 22:04:39 -0500

    Add: Ability to login and Add: LoginForm

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,28 @@
+import axios, { AxiosError } from 'axios';
+
 export async function POST(req: Request) {
-  return new Response('Current Authed User');
+  const { email, password } = await req.json();
+
+  try {
+    const res = await axios.post(
+      `${process.env.API_URL}/api/v1/auth/login`,
+      { email, password },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (res.status !== 200) return new Response('Invalid credentials');
+
+    return new Response(JSON.stringify(res.data));
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return new Response(error.response?.data, {
+        status: error.response?.status,
+      });
+    }
+    return new Response('Something went wrong', { status: 500 });
+  }
 }
---
+++ src/app/components/AdminMain.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
+
+function AdminMain({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default AdminMain;
